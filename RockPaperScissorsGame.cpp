f06f8775372cf60f2c9fd41b279852ba3fadda4e
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-08-23 16:58:25 +0300

    The UML design and the coding of the classes according to the UML were completed.

--- RockPaperScissorsGame.cpp
+++ RockPaperScissorsGame.cpp
@@ -4,23 +4,71 @@ using namespace std;
 
 class Player {
 protected:
-	string playerName;
+	string name;
+	int choice;
+	int score;
 
 public:
-	virtual int Choice() = 0;
+	string getName() {
+		return name;
+	}
+	
+	int getChoice() {
+		return choice;
+	}
+
+	int getScore() {
+		return score;
+	}
+
+	void setName(string name) {
+		this->name = name;
+	}
+
+	virtual void setChoice() = 0;
+
+	void setScore(int score) {
+		this->score = score;
+	}
 };
 
 class ComputerPlayer {
+protected:
+	int choice;
+	int score;
+
 public:
-	virtual int Choice() = 0;
+	int getChoice() {
+		return choice;
+	}
+	
+	int getScore() {
+		return score;
+	}
+
+	virtual void setChoice() = 0;
+
+	int setScore(int score) {
+		this->score = score;
+	}
+};
+
+class HumanPlayer : public Player {
+public:
+	void setChoice() {
+		cout << "Rock: 1 | Paper: 2 | Scissors: 3\nEnter your choice: ";
+		cin >> choice;
+	}
+};
+
+class RandomComputerPlayer : public ComputerPlayer {
+public:
+	void setChoice() {
+		//
+	}
 };
 
 int main(){
-
-
-
-
-
 
 
 
