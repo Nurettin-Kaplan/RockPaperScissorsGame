2aa9ff4a4fc3e67f02516e62103ab5976a86f52c
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-08-23 16:06:23 +0300

    Abstract classes were created.

--- RockPaperScissorsGame.cpp
+++ RockPaperScissorsGame.cpp
@@ -1,5 +1,19 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
+
+class Player {
+protected:
+	string playerName;
+
+public:
+	virtual int Choice() = 0;
+};
+
+class ComputerPlayer {
+public:
+	virtual int Choice() = 0;
+};
 
 int main(){
 
